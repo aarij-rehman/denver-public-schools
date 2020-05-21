fc6c11a893d7e8ac5fb4f66164782a7d2ad4c217
Aarij Rehman <aarij.rehman@gmail.com>
2020-05-21 16:03:11 -0500

    Created Segment class for segment objects

---
+++ phase1/segment.py
@@ -0,0 +1,14 @@
+# A segment class that keeps track of a prep site with it's carry in sites
+class Segment: 
+    prep = 0
+    carry1 = 0
+    carry2 = 0
+    time_window = (0, 0)
+    service_time = 0
+
+    def __init__(self, prep_, carry1_, carry2_):
+        self.prep = prep_
+        self.carry1 = carry1_
+        self.carry2 = carry2_
+
+
