0ebd079001dba8ad175c6be974b5eae361d274fd
Aarij Rehman <aarij.rehman@gmail.com>
2020-06-10 14:24:03 -0400

    Added #comments

--- phase1/graph.py
+++ phase1/graph.py
@@ -6,15 +6,21 @@ class Graph:
     distance = lambda: True
     routes = {}
 
+    # Takes a list of segments and a distance function
     def __init__(self, segments, distance):
         self.segments = segments
         self.distance = distance
         for i in (range(len(segments))):
             self.routes[i] = [i]
-        
+
+   # Determines if two segments are visited on the same routes  
     def _on_same_route(self, segment_indx1, segment_indx2):
         return segment_indx2 in self.routes[segment_indx1]
     
+    # Checks if merging two segments is feasible 
+    # 1) The segments cannot be on the same route 
+    # 2) Upon merging, a vehicle must be able to travel the route and satisfy
+    #    the time constraints of all sites
     def _merge_is_feasible(self, segment_indx1, segment_indx2):
         if self._on_same_route(segment_indx1, segment_indx2):
             return False
@@ -22,6 +28,8 @@ class Graph:
         pot_route.sort(key=lambda seg: (seg.time_window[0], seg.time_window[1]))
         return self._route_simulator(pot_route)
         
+    # Simulates a vehicle traveling along a certain route. Returns True if no time constraints 
+    # are violated. Returns false otherwise. 
     def _route_simulator(self, route):
         current_time = pd.to_datetime('5:30 AM')
         current_location = Segment(0,0,0)
@@ -37,6 +45,7 @@ class Graph:
             current_location = seg
         return True
 
+    # Merges two segments if they are feasible. Otherwise does nothing. 
     def merge(self, segment_indx1, segment_indx2):
         if self._merge_is_feasible(segment_indx1, segment_indx2):
             newroute = self.routes[segment_indx1] + self.routes[segment_indx2]
@@ -45,6 +54,7 @@ class Graph:
             for seg_indx in newroute:
                 self.routes[seg_indx] = newroute
 
+    # Retures all routes. 
     def get_routes(self):
         ret = []
         for i in self.routes:
--- phase1/segment.py
+++ phase1/segment.py
@@ -1,4 +1,6 @@
-# A segment class that keeps track of a prep site with it's carry in sites
+# Maintains the segment object. Keeps track of the prep site and carry-in sites on a segment. Also stores
+# information about the timewindow which the segment must be visited and how long it takes to service the
+# segment. 
 class Segment: 
     prep = 0
     carry1 = 0
@@ -6,6 +8,8 @@ class Segment:
     time_window = (0, 0)
     service_time = 0
 
+    # To initalize a Segment object, you need to pass in a prep and two carry-in sites. The timewindow and 
+    # service time can and should be adjusted later (You can't determine these things with just the sites). 
     def __init__(self, prep_, carry1_, carry2_):
         self.prep = prep_
         self.carry1 = carry1_
